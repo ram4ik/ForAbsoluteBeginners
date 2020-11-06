5ce3fa1a6cf101b703be231f56b64cf6a0f3dafb
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-11-06 17:36:13 +0200

    Initial Commit

---
+++ ForAbsoluteBeginners/ForAbsoluteBeginnersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForAbsoluteBeginnersApp.swift
+//  ForAbsoluteBeginners
+//
+//  Created by Ramill Ibragimov on 06.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct ForAbsoluteBeginnersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
